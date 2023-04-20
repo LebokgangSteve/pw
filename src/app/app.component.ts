350e4ee828045cf191d0d3238a6f7d305e337744
Lebokgang Mokgalaka <lebokgang@harambee.co.za>
2023-04-20 12:08:11 +0200

    Password generator

---
+++ src/app/app.component.ts
@@ -0,0 +1,78 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  length = 0;
+  includeLetters=false;
+  includeNumbers=false;
+  includeSymbols=false;
+password = "";
+
+onChangeLength(value:string){
+const parsedValue= parseInt(value);
+
+if(!isNaN(parsedValue)){
+  this.length=parsedValue;
+}
+else{
+  this.length = 0;
+  this.password = '';
+  
+}
+
+
+
+}
+onChangeUseLetters(){
+  this.includeLetters= !this.includeLetters;
+  this.password = '';
+}
+
+onChangeUseNumbers(){
+  this.includeNumbers= !this.includeNumbers;
+  this.password = '';
+}
+
+onChangeUseSymbols(){
+  this.includeSymbols= !this.includeSymbols;
+  this.password = '';
+}
+
+constructor(){
+  this.password="";
+}
+
+onButtonClick(){
+  const numbers = "1234567890";
+  const letters = "abcdefghijklmnopqrstuvwxyz";
+  const symbols = "!@#$%^&*()";
+
+  let validChars= "";
+  if(this.includeLetters){
+    validChars+= letters;
+  }
+  if(this.includeNumbers){
+    validChars+= numbers;
+  }
+ 
+  if(this.includeSymbols){
+    validChars+= symbols;
+  }
+ 
+  let generatedPassword = "";
+  for(let i = 0; i <this.length; i++){
+    const index=Math.floor(Math.random()*validChars.length);
+    generatedPassword+= validChars[index];
+  }
+
+  
+  this.password= generatedPassword;
+ 
+}
+
+}
